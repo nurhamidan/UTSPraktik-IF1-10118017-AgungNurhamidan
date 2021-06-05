db4905c79eec543995fadd1f510abbc8e787c6fa
nurhamidan <agungnurhamidan@gmail.com>
2021-06-05 10:09:23 +0700

    Commit pertama.

---
+++ app/src/main/java/com/nurhamidan/utspraktik_if1_10118017_agungnurhamidan/MainActivity.java
@@ -0,0 +1,14 @@
+package com.nurhamidan.utspraktik_if1_10118017_agungnurhamidan;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
